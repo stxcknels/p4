b2c86b8940e5015b665594531f05a49522fc6d24
stxcknels <stxcknels@outlook.com>
2023-06-15 20:28:25 +0300

    2 com

--- Untitled.py
+++ Untitled.py
@@ -21,6 +21,4 @@ def strcounter(s): #O(N)
     syms_counter = {}
     for sym in s:
         syms_counter[sym] = syms_counter.get(sym, 0) + 1
-    for sym, count in syms_counter.items():
-        print(sym, count)
-strcounter('abcnra')
+print('Изменение')
